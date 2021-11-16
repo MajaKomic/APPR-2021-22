289fe1c016989664e9e18caeb3829307563235d7
MajaKomic <komic.maja@gmail.com>
2021-11-16 17:07:57 +0100

    test na domačem računalniku

--- lib/libraries.r
+++ lib/libraries.r
@@ -6,7 +6,7 @@ library(tmap)
 library(shiny)
 library(readr)
 
-#v to mapo pišemo vse knjižnice, v ostale pa ne
+#v to mapo pišemo vse knjižnice, v ostale pa ni treba nobene
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
